6e14091fe717c1d3c841be1ad41bf9d4d09ae3be
mayukhmondal42 <mayukhmondal42@gmail.com>
2026-07-06 23:42:37 +0530

    error

--- app.js
+++ app.js
@@ -45,10 +45,14 @@ app.get("/listings/:id", async (req, res) => {
     res.render("listings/show.ejs", { listing });
 });
 
-app.post("/listings", async (req, res) => {
-    const newListing = new Listing(req.body.listing);
-    newListing.save();
-    res.redirect("/listings");
+app.post("/listings", async (req, res, next) => {
+    try {
+        const newListing = new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect("/listings");
+    } catch (err) {
+        next(err);
+    }
 });
 
 app.get("/listings/:id/edit", async (req, res) => {
@@ -92,6 +96,11 @@ app.delete("/listings/:id", async (req, res) => {
 //     console.log("sample was saved");
 //     res.send("successful testing");
 // });
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).send("Something went wrong!");
+});
 
 app.listen(8080, () => {
     console.log("server is listening to port 8080");
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).send("Something went wrong!");
+});
